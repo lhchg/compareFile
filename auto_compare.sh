74e3933ea249d258af3be64334898fe5de9c7f60
haocheng Li <lhao_cheng@163.com>
2021-03-09 11:51:35 +0800

    add auto_compare
    sh for run the compare,
    need to change <file1><file2> to your own file

---
+++ auto_compare.sh
@@ -0,0 +1,10 @@
+./compare <file1> <file2>
+#<file1> <file2> are the files need to compare
+RESULT=$?
+if [ "$RESULT" == 3 ];then
+    echo "cpu output file is different from golden"
+elif [ "$RESULT" == 2 ];then
+    echo "cpu output file is same as golden"
+elif [ "$RESULT" == 255 ];then
+    echo "cpu file compare has something wrong"
+fi
